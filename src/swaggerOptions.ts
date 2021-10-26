1f0d1f3489e08dac41f00a5bbd7e7bf41a2a6159
david.gomez <david.gomez@teamcore.cl>
2021-10-26 17:40:59 -0300

    agregando configuracion inicial de swwagger

---
+++ src/swaggerOptions.ts
@@ -0,0 +1,16 @@
+export const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title:'Security Microservice API',
+            version: '1.0.0',
+            description: 'Release 2.0 - Security Microservice'
+        },
+        servers: [
+            {
+                url: "http://localhost:3000"
+            }
+        ]
+    },
+    apis: ["./src/server/routes/*.ts"]
+}
